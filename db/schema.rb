6cfe1c3e086715f24b3967c72fc3ab625006fea4
minh la <satthutit@gmail.com>
2018-05-23 14:44:17 +0900

    Create scaffold engine

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180523054055) do
+ActiveRecord::Schema.define(version: 20180523054343) do
+
+  create_table "engines", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "models", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
